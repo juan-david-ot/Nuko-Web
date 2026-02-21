2c9a8c834dd7f04840eab0598d2a4129ed401653
JuanDa <juandarm02@gmail.com>
2026-02-22 00:22:48 +0100

    Refactor

--- eslint.config.js
+++ eslint.config.js
@@ -7,32 +7,49 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-	globalIgnores(['dist', 'node_modules', 'vite.config.ts']),
-	{
-		files: ['**/*.{ts,tsx}'],
-		extends: [
-			js.configs.recommended,
-			tseslint.configs.recommended,
-			react.configs.flat.recommended,
-			reactHooks.configs.flat.recommended,
-			reactRefresh.configs.vite
-		],
-		languageOptions: {
-			parser: tseslint.parser,
-			parserOptions: {
-				project: ['./tsconfig.app.json']
-			},
-			ecmaVersion: 'latest',
-			globals: globals.browser
-		},
-		settings: {
-			react: {
-				version: 'detect'
-			}
-		},
-		rules: {
-			'@typescript-eslint/no-unused-vars': 'warn',
-			'react/react-in-jsx-scope': 'off'
-		}
-	}
+    globalIgnores(['build', 'node_modules', 'vite.config.ts']),
+    {
+        files: ['**/*.{js,jsx}'],
+        extends: [js.configs.recommended],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            globals: globals.browser
+        }
+    },
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            react.configs.flat.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite
+        ],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: ['./tsconfig.app.json']
+            },
+            ecmaVersion: 'latest',
+            globals: globals.browser
+        },
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'react/react-in-jsx-scope': 'off',
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'never'],
+            'indent': ['error', 4],
+            'brace-style': ['error', 'stroustrup'],
+            'comma-dangle': ['error', 'never'],
+            'object-curly-spacing': ['error', 'always'],
+            'array-bracket-spacing': ['error', 'never'],
+            'keyword-spacing': ['error', { before: true, after: true }],
+            'space-before-blocks': ['error', 'always']
+        }
+    }
 ])
--- src/definitions/enums.ts
+++ src/definitions/enums.ts
@@ -1,3 +1,3 @@
 export enum TestEnum {
-	test = 'test'
+    test = 'test'
 }
