155febe8912cdc164745913df03084a125d7d11b
JuanDa <juandarm02@gmail.com>
2026-03-04 00:11:47 +0100

    First react-router-dom implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { ThemeProviderWrapper } from './contexts/theme.context.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
     <ThemeProviderWrapper>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </ThemeProviderWrapper>
 )
