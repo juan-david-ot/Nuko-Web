7151bcf3fcceae2fe06bcb115ed790cc42b3a466
Juan David Orozco Torres <juandarm02@gmail.com>
2026-03-04 00:03:45 +0100

    Disabled strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { ThemeProviderWrapper } from './contexts/theme.context.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <ThemeProviderWrapper>
-            <App />
-        </ThemeProviderWrapper>
-    </StrictMode>
+    <ThemeProviderWrapper>
+        <App />
+    </ThemeProviderWrapper>
 )
