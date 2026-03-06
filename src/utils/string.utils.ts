4e53b6b23d2133daa28154eee6e4ff929d8ce9d7
Juan David Orozco Torres <juandarm02@gmail.com>
2026-03-06 14:06:53 +0100

    Refactor

--- src/utils/string.utils.ts
+++ src/utils/string.utils.ts
@@ -1,7 +1,12 @@
-export function stringUtilFunction1(): void {
+function stringUtilFunction1(): void {
     console.log('stringUtilFunction1')
 }
 
-export function stringUtilFunction2(): void {
+function stringUtilFunction2(): void {
     console.log('stringUtilFunction2')
+}
+
+export {
+    stringUtilFunction1,
+    stringUtilFunction2
 }
