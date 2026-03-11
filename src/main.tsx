faf11d9912af2a008b57ff4dceef919cbb785efb
JuanDa <juandarm02@gmail.com>
2026-03-11 21:03:30 +0100

    Refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import App from './App.tsx'
 import { ThemeProviderWrapper } from './contexts/theme.context.tsx'
+import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
