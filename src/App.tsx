bae1d07c0ac509800b953731eeaff12a52bcd1c0
JuanDa <juandarm02@gmail.com>
2026-03-11 21:33:34 +0100

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Button } from '@heroui/react'
 import { useTheme } from './contexts/useTheme'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App(): JSX.Element {
     const [count, setCount] = useState(0)
