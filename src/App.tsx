e213922e6dd4351a272fcb7bd486cea03b04f646
JuanDa <juandarm02@gmail.com>
2026-03-13 23:57:56 +0100

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar'
 
 function App() {
     return (
-        <main className='m-0 h-screen flex flex-col-reverse justify-between items-center bg-background text-foreground'>
+        <main className='m-0 h-screen flex flex-col-reverse justify-between items-center lg:flex-row lg:justify-start lg:items-start bg-background text-foreground'>
             <title>Nuko</title>
             <nav className='m-1'>
                 <Navbar />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,26 @@
 import { Tabs } from '@heroui/react'
+import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router'
 
 function Navbar() {
     const location = useLocation()
     const navigate = useNavigate()
+
+    const [isDesktop, setIsDesktop] = useState(() => window.matchMedia('(min-width: 1024px)').matches)
+
+    useEffect(() => {
+        const media = window.matchMedia('(min-width: 64rem)')
+
+        const listener = (e: MediaQueryListEvent) => setIsDesktop(e.matches)
+        media.addEventListener('change', listener)
+
+        return () => media.removeEventListener('change', listener)
+    }, [])
+
     return (
         <Tabs
             className="w-full max-w-md"
+            orientation={isDesktop ? 'vertical' : 'horizontal'}
             selectedKey={location.pathname}
             onSelectionChange={(key) => navigate(String(key))}
         >
