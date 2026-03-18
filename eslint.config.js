2d774289bef612e63537d65517b8b1e0ff0f84d3
JuanDa <juandarm02@gmail.com>
2026-03-19 00:02:26 +0100

    Allow explicit any

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,7 @@ export default defineConfig([
             }
         },
         rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': 'warn',
             'react/react-in-jsx-scope': 'off',
             'react-hooks/exhaustive-deps': 'off',
